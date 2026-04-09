2bdc11c43ac9910cd7e8e2f2e491fe7899216fb7
mihai-bandur <mihai.bandur@student.tuiasi.ro>
2026-04-09 23:55:38 +0300

    rebase the codebase

--- wav_recorder/ui.c
+++ wav_recorder/ui.c
@@ -2,10 +2,74 @@
 #include "lvgl.h"
 
 // Păstrăm un pointer către opt1 ca să o putem modifica mai târziu din exterior
-static lv_obj_t *opt1;
+
+
+lv_obj_t *opt1;
+
+static lv_obj_t* create_rectangle(lv_obj_t* parent, uint8_t width, uint8_t height, lv_align_t align, uint8_t x_ofs, uint8_t y_ofs, uint32_t color){
+    lv_obj_t* new_rectangle = lv_obj_create(parent);
+    
+    lv_obj_set_size(new_rectangle, width, height);
+
+    lv_obj_align(new_rectangle, align, x_ofs, y_ofs);
+
+    lv_obj_set_style_bg_color(new_rectangle, lv_color_hex(color), 0);
+
+    return new_rectangle;
+
+}
+
+static lv_obj_t* create_progress_bar(lv_obj_t* parent, uint8_t width, uint8_t height,lv_align_t align,uint8_t x_ofs, uint8_t y_ofs,  bool is_animated){
+    lv_obj_t * new_bar = lv_bar_create(parent);
+
+    lv_obj_set_size(new_bar, width, height);
+
+    lv_obj_center(new_bar);
+
+    lv_obj_align(new_bar, align, x_ofs, y_ofs);
+
+    if(is_animated)
+        lv_bar_set_value(new_bar, 0, LV_ANIM_ON);
+    else
+        lv_bar_set_value(new_bar, 0, LV_ANIM_OFF);
+
+    return new_bar;
+}
+static lv_obj_t* create_label(lv_obj_t* parent,  const char* text, const lv_font_t* font ,uint32_t color, lv_align_t align, int16_t x_ofs, int16_t y_ofs){
+    
+    lv_obj_t *new_label = lv_label_create(parent);    
+    lv_label_set_text(new_label, text);
+
+    if(font != NULL){
+        lv_obj_set_style_text_font(new_label, font, 0);
+    }
+
+    lv_obj_set_style_text_color(new_label, lv_color_hex(color), 0);
+    lv_obj_align(new_label, align, x_ofs, y_ofs);
+
+    return new_label;
+
+}
+
+static lv_obj_t* create_button(int16_t pos_x, int16_t pos_y, uint16_t width, uint16_t height, uint32_t color ,const char* text){
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());
+    lv_obj_set_pos(btn, pos_x, pos_y);
+    lv_obj_set_size(btn, width, height);
+    lv_obj_set_style_bg_color(btn , lv_color_hex(color), 0);    
+
+    lv_obj_t* label = lv_label_create(btn);
+    lv_label_set_text(label, text);
+    lv_obj_center(label);
+
+    return btn;
+}
+
+
 
 void ui_init(void) {
     // fundal titlu
+
+    create_rectangle(lv_scr_act(), 240, 120,  LV_ALIGN_TOP_MID, 0,0, 0xFFFFFF);
     lv_obj_t * my_rect = lv_obj_create(lv_scr_act());
     lv_obj_set_size(my_rect , 240, 120);
     lv_obj_align(my_rect, LV_ALIGN_TOP_MID, 0, 0);
@@ -18,30 +82,16 @@ void ui_init(void) {
     lv_obj_set_style_text_color(symbol, lv_color_hex(0x000000), 0);
     lv_obj_align(symbol, LV_ALIGN_TOP_LEFT, 0, 35);
 
-    lv_obj_t *label = lv_label_create(my_rect);
-    lv_label_set_text(label, "Echo Note");
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_32, 0);
-    lv_obj_set_style_text_color(label, lv_color_hex(0x000000), 0);
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 25, 35);
+    lv_obj_t *label = create_label(my_rect, "Echo Note", &lv_font_montserrat_32, 0X000000, LV_ALIGN_TOP_MID, 25, 35);
 
     // optiuni 
-    opt1 = lv_label_create(lv_scr_act());
-    lv_label_set_text(opt1, "1. Inregistrare voce");
-    lv_obj_set_style_text_font(opt1, &lv_font_montserrat_20, 0);
-    lv_obj_set_style_text_color(opt1, lv_color_hex(0xFFFFFF), 0);
-    lv_obj_align(opt1, LV_ALIGN_CENTER, 0, 0);
+    opt1 = create_label(lv_scr_act(), "1. Inregistare voce", &lv_font_montserrat_20, 0XFFFFFF, LV_ALIGN_CENTER, 0, 0);
 
-    lv_obj_t *opt2 = lv_label_create(lv_scr_act());
-    lv_label_set_text(opt2, "2. Ascultare");
-    lv_obj_set_style_text_font(opt2, &lv_font_montserrat_20, 0);
-    lv_obj_set_style_text_color(opt2, lv_color_hex(0xFFFFFF), 0);
-    lv_obj_align(opt2, LV_ALIGN_CENTER, 0, 30);
+    lv_obj_t *opt2 = create_label(lv_scr_act(), "2. Ascultare", &lv_font_montserrat_20, 0XFFFFF, LV_ALIGN_CENTER, 0, 30);
 
-    lv_obj_t *opt3 = lv_label_create(lv_scr_act());
-    lv_label_set_text(opt3, "3. Oprire");
-    lv_obj_set_style_text_font(opt3, &lv_font_montserrat_20, 0);
-    lv_obj_set_style_text_color(opt3, lv_color_hex(0xFFFFFF), 0);
-    lv_obj_align(opt3, LV_ALIGN_CENTER, 0, 60);
+    lv_obj_t *opt3 = create_label(lv_scr_act(), "3. Oprire", &lv_font_montserrat_20, 0XFFFFF, LV_ALIGN_CENTER, 0, 60);
+
+    lv_obj_t * test_btn = create_button(70, 230, 100, 40, 0xFF0000, "TEST");
 
     // bar
     static lv_style_t style_bg;
@@ -75,6 +125,5 @@ void ui_set_opt1_active(bool is_active) {
     } else {
         lv_obj_set_style_text_color(opt1, lv_color_hex(0xFFFFFF), 0);
     }
-    // Fortam ecranul sa redeseneze optiunea
     lv_obj_invalidate(opt1);
 }
--- wav_recorder/ui.h
+++ wav_recorder/ui.h
@@ -1,11 +1,14 @@
 #ifndef UI_H_
 #define UI_H_
 
-#include<stdbool.h>
+#include "commons.h"
+#include"lvgl.h"
 
 void ui_init(void);
 
 
 void ui_set_opt1_active(bool is_active);
+
+
 
 #endif
